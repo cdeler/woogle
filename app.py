77f4993bfaf3572cef91a8091847f2eb01a36c7b
Klimenko Andrei <zaktoq@yandex.ru>
2018-07-10 16:32:41 +0300

    #6 fixed pull request issues

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ application = Flask(__name__)
 
 @application.route("/<string:ping>", methods=["GET"])
 def get_tasks(ping):
-    return "pong" if ping == "ping" else abort(404) #jsonify({'pong': "0"})
+    return "pong" if ping == "ping" else abort(400) 
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=80)
