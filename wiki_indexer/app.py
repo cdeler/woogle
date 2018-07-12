7d4e311d0d3af6e0896a81d7ab6dabb11a970292
Klimenko Andrei <zaktoq@yandex.ru>
2018-07-12 17:09:01 +0300

    #18 Clear thr repository

---
+++ wiki_indexer/app.py
@@ -0,0 +1,12 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+from flask import abort
+
+application = Flask(__name__)
+
+@application.route("/<string:ping>", methods=["GET"])
+def get_tasks(ping):
+    return "pong" if ping == "ping" else abort(400)
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=80)
