ad1a6218e74d34e45dc1114718e58a175a553bd5
Klimenko Andrei <zaktoq@yandex.ru>
2018-07-24 18:07:56 +0300

    #32 List view for admin panal

--- backend_application/app/wikisearch/admin.py
+++ backend_application/app/wikisearch/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article
+
+
 
 from wikisearch.models import Article
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = (
+        'title',
+        'url',
+        'text',
+        'page_rank'
+    )
