6d5c05848199dc293d83e9623473be4b4f43a6ae
qwerty <erntimur@gmail.com>
2018-07-25 05:10:14 +0300

    #29 db binding v1 is implemented

--- crawler/WikiSpider.py
+++ crawler/WikiSpider.py
@@ -7,7 +7,8 @@ class WikiSpider(scrapy.Spider):
     name = 'WikiSpider'
 
     start_urls = [
-        'https://ru.wikipedia.org/w/index.php?title=%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0%92%D1%81%D0%B5_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D1%8B',
+        'https://ru.wikipedia.org/wiki/%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9_%D1%8F%D0%B7%D1%8B%D0%BA',
+        # 'https://ru.wikipedia.org/w/index.php?title=%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0%92%D1%81%D0%B5_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D1%8B',
     ]
 
     allowed_domains = ['ru.wikipedia.org', ]
---
+++ crawler/database_binding.py
@@ -0,0 +1,38 @@
+from models import Article, base
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+db_string = "postgres://postgres:postgres@localhost:5432/hello_world"
+db = create_engine(db_string)
+
+Session = sessionmaker(db)
+session = Session()
+base.metadata.create_all(db)
+
+
+def insert(*args, **kwargs):
+    session.add(Article(*args, **kwargs))
+    session.commit()
+
+
+def delete_all():
+    session.query(Article).delete()
+    session.commit()
+
+
+def update():
+    pass
+
+
+def read():
+    articles = session.query(Article)
+    for article in articles:
+        print(article.title)
+
+
+if __name__ == '__main__':
+    insert(title="some title", url="some url", text='some text')
+    insert(title="some title 2", url="some url 2", text='some text 2')
+    read()
+    delete_all()
