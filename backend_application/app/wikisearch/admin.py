5d0321151191f0516ff540ce4416cb518618b6af
Klimenko Andrei <zaktoq@yandex.ru>
2018-07-30 17:27:24 +0300

    #39 actions for admin panel has added

--- backend_application/app/wikisearch/admin.py
+++ backend_application/app/wikisearch/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+import requests
 # Register your models here.
 from .models import Article
 
@@ -16,3 +16,20 @@ class ArticleAdmin(admin.ModelAdmin):
         'text',
         'page_rank'
     )
+
+    actions = ['reindex','reparse']
+
+    def reindex(self, request, queryset):
+        result = True
+        for row in queryset.values('id'):
+            response = requests.post("http://0.0.0.0:9999", data = str(row['id']))
+            if response.content.decode('ascii') != f'{str(row["id"])} is executed':
+                result = False
+        if result:
+            self.message_user(request, 'Articles ware successfully indexed')
+
+    def reparse(self, request, queryset):
+        pass
+
+    reindex.short_description = 'Put article in elastic again'
+    reparse.short_description = 'Parse article again in database'
