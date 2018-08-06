2e4a536b16a1be7443da8e9d0f33a94464d0d87b
Klimenko Andrei <zaktoq@yandex.ru>
2018-08-06 17:10:35 +0300

    #59 pagerank computes

--- crawler/database_binding.py
+++ crawler/database_binding.py
@@ -1,38 +1,105 @@
-from models import Article, base
+from .models import Article, base
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from scrapy.http import HtmlResponse
+import requests
 
-db_string = "postgres://username:password@localhost/dbname"
-db = create_engine(db_string)
+from .WikiResponseProcessor import *
 
-Session = sessionmaker(db)
-session = Session()
-base.metadata.create_all(db)
+def init_db():
+    db_string = "postgresql:///test"
+    db = create_engine(db_string)
+
+    Session = sessionmaker(db)
+    session = Session()
+    base.metadata.create_all(db)
+    return session
 
 
-def insert(*args, **kwargs):
+def insert(session, *args, **kwargs):
     session.add(Article(*args, **kwargs))
     session.commit()
 
 
-def delete_all():
+def delete_all(session):
     session.query(Article).delete()
     session.commit()
 
 
-def update():
-    pass
+def update(session, id, title, url, text):
+    session.query(Article).filter(Article.id == id).update(
+        {'title': title, 'url': url, 'text': text})
+    session.commit()
 
 
-def read():
+def reparse_by_id(session, id):
+    # only for database
+    url = session.query(Article.url).filter(Article.id == id).first()[0]
+    response = requests.get(url)
+    response = HtmlResponse(url=url, body=response.content)
+    DBResponseProcessor().process(response, id_to_update=id)
+
+
+def get_rows(ses):
+    """
+    Function to get amount of rows in a table.
+
+    :param session: session establishes all conversations with the database and represents a “holding zone”.
+    :type session: sqlalchemy.session
+    :returns: integer amount of rows in table
+    """
+    return ses.query(Article).count()
+
+def get_urls(session):
+    """
+    Function to get all urls of article in a table.
+
+    :param session: session establishes all conversations with the database and represents a “holding zone”.
+    :type session: sqlalchemy.session
+    :returns: integer amount of rows in table
+    """
+    url = session.query(Article.url)
+    res = [u[0] for u in url]
+    return res
+
+def get_links_url(session, url):
+    """
+    Function to get all urls that referred on other article.
+
+    :param session: session establishes all conversations with the database and represents a “holding zone”.
+    :type session: sqlalchemy.session.
+    :param url: url of article with dependicies.
+    :type url: str.
+    :returns: list of strings - list of urls
+    """
+    url = session.query(Article.urls).filter(Article.url == url)
+    return [u[0].split() for u in url][0]
+
+def update_page_rank(session, url, pagerank):
+    """
+    Function to update page rank by id.
+
+    :param session: session establishes all conversations with the database and represents a “holding zone”.
+    :type session: sqlalchemy.session.
+    :param url: url of article.
+    :type url: str.
+    :param pagerank: new pagerank for artiocle.
+    :type pagerank: float
+    :returns: None
+    """
+    url = session.query(Article.page_rank).filter(Article.url == url).update({
+        'page_rank': pagerank
+    })
+    session.commit()
+
+def read(session):
     articles = session.query(Article)
     for article in articles:
         print(article.title)
 
 
 if __name__ == '__main__':
-    insert(title="some title", url="some url", text='some text')
-    insert(title="some title 2", url="some url 2", text='some text 2')
-    read()
-    delete_all()
+    session = init_db()
+    #insert(session, title='Sodapoppin', url='https://en.wikipedia.org/wiki/Sodapoppin',text='Thomas Jefferson Chance Morris IV (born February 15, 1994), more commonly known by his online alias Sodapoppin, a Twitch.tv streamer and former World of Warcraft player. He has among the largest following on Twitch with over 2 million followers and over 200 million views',page_rank='0')
+    reparse_by_id(session, 11)
---
+++ crawler/pagerank.py
@@ -0,0 +1,108 @@
+import itertools
+import numpy as np
+from crawler.database_binding import init_db, get_urls, get_links_url, get_rows, update_page_rank
+from time import time
+
+
+def pageRank(M, d=0.85, squere_error=1e-6):
+    """
+    Function to get pagerank of the matrix.
+
+    :param M: matrix that represent graph of urls with dependicies.
+    :type M: np.array
+    :param d: damping factor.
+    :type d: float.
+    :param squere_error: difference between  two successive PageRank vectors.
+    :type squere_error: float.
+    :returns: lists of pageranks.
+    """
+    n_pages = M.shape[0]
+    v = np.random.rand(n_pages)
+
+    v = v/ v.sum()
+
+    last_v = np.ones((n_pages))
+    M_hat =  d*M + (1-d)/n_pages * np.ones((n_pages, n_pages))
+    while np.square(v - last_v).sum() > squere_error:
+        last_v = v
+        v = M_hat.dot(v)
+    return v
+
+ses = init_db()
+
+
+def create_graph(ses):
+    """
+    Function that create matrix from database.
+
+    :param ses: session with database.
+    :type sqlalchemy.session.
+    :return: Graph with dependices.
+    """
+    names = get_urls(ses)
+    x = np.zeros((3,), dtype={'names': names, 'formats': list(itertools.repeat('f4', len(names)))})
+    # for i in names:
+    #     links = get_links_url(ses, i)
+    #     print(links)
+    #     for k in links:
+    #         if k in names:
+    #             x[i][names.index(k)] = 1
+    #     !!!!!!!!!!!!!!!!!Atempt2!!!!!!!!!!!!!!
+    for i in names:
+        links = set(get_links_url(ses, i))
+        dependings = links & set(names)
+        while dependings:
+            item = dependings.pop()
+            x[i][names.index(item)] = 1
+
+    # !!!!!!!!!!!!!!!Attempt 3!!!!!!!!!!!!
+    # ns = set(names)
+    # namse = {i: get_links_url(ses, i) for i in names}
+    # for i, j in namse.items():
+    #     temp = set(j) & ns
+    #     while temp:
+    #         item = temp.pop()
+    #         x[i][names.index(item)] = 1
+    print(x)
+    return x
+
+def convert_to_array(arr):
+    """
+    Function that convert list of tuple array into list of lists array.
+
+    :param arr: array that needs to be converted.
+    :type arr: np.array
+    :return: np.array - new
+    """
+    return np.asarray([list(i) for i in arr.tolist()])
+
+def get_probabilyties(session, new_x):
+    """
+    Function that equally distribute probability of each vector.
+
+    :param session: session with database.
+    :type session: sqlalchemy.session.
+    :param new_x: right array.
+    :type new_x: np.array.
+    :return: matrix.
+    """
+    r = get_rows(session)
+    M = np.empty((r, r))
+    for i in range(r):
+        np.set_printoptions(precision=3)
+        M[:, i] = new_x[:, i] / new_x[:, i].sum()
+    print(M)
+    return M
+
+
+def main():
+    rate = dict(zip(get_urls(ses), pageRank(get_probabilyties(ses, convert_to_array(create_graph(ses))))))
+    for i, j in rate.items():
+        update_page_rank(ses, i, j)
+
+if __name__ == '__main__':
+
+    start = time()
+    result = main()
+    duration = time() - start
+    print(result, duration)
