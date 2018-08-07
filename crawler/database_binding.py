4a815467b75783cfff853c00bb3b2556f233ced9
Catherine <191887@niuitmo.ru>
2018-08-07 11:19:54 +0300

    update database_binding.py

--- crawler/database_binding.py
+++ crawler/database_binding.py
@@ -2,37 +2,87 @@ from models import Article, base
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from scrapy.http import HtmlResponse
+import requests
 
-db_string = "postgres://username:password@localhost/dbname"
-db = create_engine(db_string)
-
-Session = sessionmaker(db)
-session = Session()
-base.metadata.create_all(db)
+import WikiResponseProcessor
 
 
-def insert(*args, **kwargs):
+def init_db():
+    db_string = "postgres://username:password@localhost/dbname"
+    db = create_engine(db_string)
+
+    Session = sessionmaker(db)
+    session = Session()
+    base.metadata.create_all(db)
+    return session
+
+
+def read(session, id=None, title=None, url=None):
+    if id:
+        return session.query(
+            Article.title,
+            Article.url,
+            Article.text).filter(
+            Article.id == id).first()
+    elif title:
+        return session.query(
+            Article.title,
+            Article.url,
+            Article.text).filter(
+            Article.title == title).first()
+    elif url:
+        return session.query(
+            Article.title,
+            Article.url,
+            Article.text).filter(
+            Article.url == url).first()
+    else:
+        articles = session.query(Article.title, Article.url, Article.text)
+        return articles
+
+
+def insert(session, *args, **kwargs):
     session.add(Article(*args, **kwargs))
     session.commit()
 
 
-def delete_all():
-    session.query(Article).delete()
+def update(session, id, title, url, text):
+    session.query(Article).filter(Article.id == id).update(
+        {'title': title, 'url': url, 'text': text})
     session.commit()
 
 
-def update():
-    pass
+def reparse_by_id(session, id):
+    url = session.query(Article.url).filter(Article.id == id).first()[0]
+    response = requests.get(url)
+    response = HtmlResponse(url=url, body=response.content)
+    WikiResponseProcessor.DBResponseProcessor().process(response, id_to_update=id)
 
 
-def read():
-    articles = session.query(Article)
-    for article in articles:
-        print(article.title)
+def delete(session, id=None, title=None, url=None):
+    if id:
+        session.query(Article.id).filter(Article.id == id).delete()
+    elif title:
+        session.query(Article.title).filter(Article.title == title).delete()
+    elif url:
+        session.query(Article.url).filter(Article.url == url).delete()
+    else:
+        session.query(Article).delete()
+    session.commit()
 
 
 if __name__ == '__main__':
-    insert(title="some title", url="some url", text='some text')
-    insert(title="some title 2", url="some url 2", text='some text 2')
-    read()
-    delete_all()
+    session = init_db()
+    # dict = {'title':"some title", 'url':"some url", 'text':'some text'}
+    # insert(session, **dict)
+    # columns = ('title', 'url', 'text')
+    # input_data = ('some title', 'some url', 'some text')
+    # input_row = {k: v for k, v in zip(columns, input_data)}
+    # id = 500
+    # insert(session, id=id, **input_row)
+    # insert(session, title="some title", url="some url", text='some text')
+    reparse_by_id(session, 345)
+    # print(read(session, title='some title'))
+    # print(read(session, id=5))
+# delete(session, id=349)
