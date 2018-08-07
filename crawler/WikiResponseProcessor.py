5171e0329d41737eca2cb2fbefce98b92c46cd90
Catherine <191887@niuitmo.ru>
2018-08-07 11:01:03 +0300

    update WikiResponseProcessor.py

--- crawler/WikiResponseProcessor.py
+++ crawler/WikiResponseProcessor.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 from abc import ABC, abstractmethod
-import os
-import psycopg2
+import os, errno
+
+import database_binding
 
 
 class WikiResponseProcessor(ABC):
@@ -12,6 +13,7 @@ class WikiResponseProcessor(ABC):
 
     @staticmethod
     def getWikiResponseProcessor(args=None):
+      
         if args is not None and 'output' in args:
             mode_output = args['output']
 
@@ -20,33 +22,40 @@ class WikiResponseProcessor(ABC):
             elif mode_output == 'directory':
                 return FileWikiResponseProcessor()
             elif mode_output == 'db':
-                # code for output in db
-                print("Output mode: data base")
-                pass
+                return DBResponseProcessor()
+
             else:
                 raise ValueError(
                     f"Invalid mode output - {args['output']}. Correct value of argument 'output' - stdout, db, directory ")
         else:
             return FileWikiResponseProcessor()
-        elif processor_type == 'DBWRP':
-            return DBResponseProcessor()
 
 
 class FileWikiResponseProcessor(WikiResponseProcessor):
+    """ Class, which allows to store crawled data in files   """
+
     def process(self, response, path=os.path.join(os.getcwd(), 'texts')):
         """ Method that prints article's snippet to file
-        :param response:
-        :param path:
-        :return:
+        :param response: response to parse
+        :param path: folder to save articles to
+        :return: None
         """
-        path = os.path.join(
-            path, f"{response.xpath('//title/text()').extract_first()}.txt")
+
+        if not os.path.exists(path):
+            try:
+                os.makedirs(path)
+            except OSError as exc:
+                if exc.errno != errno.EEXIST:
+                    raise
+
+        path = os.path.join(path, f"{response.xpath('//title/text()').extract_first()}.txt")
         with open(path, 'w', encoding="utf-8") as output:
             try:
                 text = response.xpath(
                     '//div[@class="mw-parser-output"]').extract()[0]
             except IndexError:
                 return 0
+
             soup = BeautifulSoup(text, 'lxml')
             paragraph = soup.select('div.mw-parser-output > p')
             for p in paragraph:
@@ -56,13 +65,14 @@ class FileWikiResponseProcessor(WikiResponseProcessor):
 
 
 class StdOutWikiResponseProcessor(WikiResponseProcessor):
+    """ Class, which allows to print crawled data to standard output   """
+
     def process(self, response, n=40, silent=False):
         """ Method that prints first n symbols of wiki article to stdout
-
-        :param response:
-        :param n:
+        :param response: response to parse
+        :param n: number of symbols to print
         :param silent:
-        :return:
+        :return: None
         """
         output = ''
         n = int(n)
@@ -71,11 +81,53 @@ class StdOutWikiResponseProcessor(WikiResponseProcessor):
                 '//div[@class="mw-parser-output"]').extract()[0]
         except BaseException:
             return 0
+
         soup = BeautifulSoup(text, 'lxml')
         paragraph = soup.select('div.mw-parser-output > p')
         for p in paragraph:
             output += p.text
-            if len(output) > n or p.nextSibling.name != 'p' :
+            if len(output) > n or p.nextSibling.name != 'p':
                 break
+
         if not silent:
             print("\n------\n", response.url, '\n', output[:n], "\n------\n")
+
+
+class DBResponseProcessor(WikiResponseProcessor):
+    """ Class, which allows to store crawled data in database   """
+
+    def process(self, response, db=True, id_to_update=None):
+        """ Method that stores data into database
+        :param response: response to parse
+        :param db:
+        :param id_to_update: id of record that needs to be updated
+        :return: None
+        """
+        title = response.xpath('//title/text()').extract_first()
+        url = response.url
+        content = ''
+        try:
+            text = response.xpath(
+                '//div[@class="mw-parser-output"]').extract()[0]
+        except IndexError:
+            return 0
+
+        soup = BeautifulSoup(text, 'lxml')
+        paragraph = soup.select('div.mw-parser-output > p')
+        for p in paragraph:
+            content += p.text
+            if p.nextSibling.name != 'p':
+                break
+
+        session = database_binding.init_db()
+
+        if id_to_update:
+            database_binding.update(
+                session,
+                id_to_update,
+                title=title,
+                url=url,
+                text=content)
+        else:
+            database_binding.insert(
+session, title=title, url=url, text=content)
