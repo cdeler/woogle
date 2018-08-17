6ba80b574e76612d26a6fa47a821027c8cce26e6
ONosova <nosova-olenka@mail.ru>
2018-08-17 16:40:03 +0300

    add tests

---
+++ crawler/tests/test_pagerank.py
@@ -0,0 +1,34 @@
+import unittest
+import numpy as np
+from unittest.mock import create_autospec
+
+from crawler.src import pagerank
+
+
+class TestPagerank(unittest.TestCase):
+
+    def test_create_graph_positive(self):
+
+        mock_function = create_autospec(pagerank.create_graph, return_value = [(0.0, 0.0, 0.0), (1.0, 1.0, 1.0), (0.0, 0.0, 0.0)])
+        self.assertEqual(mock_function('ses'),
+                         [(0.0, 0.0, 0.0), (1.0, 1.0, 1.0), (0.0, 0.0, 0.0)])
+
+
+    def test_get_probabilities(self):
+        self.assertEqual(pagerank.get_probabilities(3, np.array([(0, 0, 1), (1, 1, 0), (1, 0, 0)])).tolist(),
+                         [[0.0, 0.0, 1.0], [0.5, 1.0, 0.0], [0.5, 0.0, 0.0]])
+
+
+    def test_pagerank(self):
+        self.assertAlmostEqual(pagerank.pageRank(
+            np.array([(0, 0, 0), (0, 0, 0), (0, 0, 1)])).tolist()[0], 0.0, 1)
+        self.assertAlmostEqual(pagerank.pageRank(
+            np.array([(0, 0, 0), (0, 0, 0), (0, 0, 1)])).tolist()[1], 0.0, 1)
+        self.assertAlmostEqual(pagerank.pageRank(
+            np.array([(0, 0, 0), (0, 0, 0), (0, 0, 1)])).tolist()[2], 0.0, 1)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
