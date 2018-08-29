2156d8f0b8f4e282c6e950d3aefd6518a53b9855
ONosova <nosova-olenka@mail.ru>
2018-08-29 18:37:45 +0300

    fix dockerfile

--- backend_application/backup.sh
+++ backend_application/backup.sh
@@ -1,1 +1,1 @@
-postgres pg_dump -U postgres -F t db_name | gzip >/backups/db-$(date +%Y-%m-%d).tar.gz
+postgres pg_dump -U postgres -F t test | gzip >/backups/db-$(date +%Y-%m-%d).tar.gz
--- backend_application/cmd.sh
+++ backend_application/cmd.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 set -e
-
 if [ "$ENV" = 'DEV' ]; then
   echo "Running Development Server"
   exec python "manage.py" runserver 0.0.0.0:8000
 else
   echo "Running Production Server"
   exec uwsgi --ini /app/woogle/uwsgi.ini --http :8000
-
+  exec 0 * * * * /backup.sh
 fi
