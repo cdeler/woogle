458e4281dd41f39482cbf934e0c0bed9bf9013a0
gsfdfsdfsdcsd <erntimur@gmail.com>
2018-08-30 17:37:52 +0300

    fix

--- backend_application/backup.sh
+++ backend_application/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Backup postgres"
-postgres pg_dump -U postgres -F t test | gzip >/backups/db-$(date +%Y-%m-%d).tar.gz
+pg_dump -U postgres -F t postgres | gzip >/backups/db-$(date +%Y-%m-%d).tar.gz
