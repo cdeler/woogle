bd1e83ad39da08b85597562ce846c22d91cbc573
Nikita Gold <nikneymn15@gmail.com>
2018-09-25 15:26:01 +0300

    delete docker-compose

--- PageRank/install_spark.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#exec ./venv/lib/python3.6/site-packages/pyspark/bin/pyspark --packages graphframes:graphframes:0.5.0-spark2.1-s_2.11
-
-#cp -r /home/ubuntu/.ivy2/jars/ /home/ubuntu/Woogle_AWS/indexer/venv/lib/python3.6/site-packages/pyspark/bin/pyspark/jars
